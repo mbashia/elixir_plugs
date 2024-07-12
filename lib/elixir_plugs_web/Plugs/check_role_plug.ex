51a4f0d6f1741b4ce37d41ecfa5701140474f0cf
mbashia <mbashiavictor@gmail.com>
2024-07-12 13:12:50 +0300

    added check role plug

--- lib/elixir_plugs_web/Plugs/check_role_plug.ex
+++ lib/elixir_plugs_web/Plugs/check_role_plug.ex
@@ -26,20 +26,4 @@ defmodule ElixirPlugsWeb.CheckRolePlug do
     end
   end
 
-  # defp has_role?(%User{} = user, roles) when is_list(roles) do
-  #   IO.inspect(roles)
-  #   Enum.any?(roles, &has_role?(user, &1))
-  # end
-
-  # defp has_role?(%User{usertype: role}, role), do: true
-  # defp has_role?(_user, _role), do: false
-
-  # defp maybe_halt(true, conn), do: conn
-
-  # defp maybe_halt(false, conn) do
-  #   conn
-  #   |> Controller.put_flash(:info, "Welcome Back")
-  #   |> Controller.redirect(to: "/companyregistration/information")
-  #   |> halt()
-  # end
 end
